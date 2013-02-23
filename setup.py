e215f04a3378b21c1464808c7bf64d8050b92773
Jernej Virag <jernej@virag.si>
2013-02-23 12:56:59 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="PySolarized",
+      version="1.0",
+      description="Simple Solr python bridge",
+      author="Jernej Virag",
+      author_email="jernej@virag.si",
+      packages=["pysolarized"],
+      requires=["requests"])
