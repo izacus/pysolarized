2c19b7f2591eaca9c15282a6e21b74a6e9ac85cc
Jernej Virag <jernej@virag.si>
2013-03-01 22:50:59 +0100

    Added from_solr_date function

--- pysolarized/__init__.py
+++ pysolarized/__init__.py
@@ -1,4 +1,5 @@
 from solr import Solr, SolrException, SolrResults
+from datetime import datetime
 
 __all__ = ["Solr", "SolrException", "SolrResults"]
 
@@ -6,3 +7,6 @@ __all__ = ["Solr", "SolrException", "SolrResults"]
 # Converts passed timezone-aware datetime object to ISO8601 format Solr expects
 def to_solr_date(date):
     return date.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+def from_solr_date(date_string):
+    return datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%SZ")
